c963987028bb004fc1045664d962201378f98efb
Mayur Nayak <nayak.mayur22@gmail.com>
2023-05-18 17:30:25 -0400

    remove auth

--- server/routes/drone.routes.js
+++ server/routes/drone.routes.js
@@ -4,9 +4,9 @@ const {authenticate} = require('../config/jwt.config')
 module.exports = app => {
     app.get('/api/allPhotos', DroneController.findAllPhotos);
 
-    app.get('/api/myPhotos',authenticate, DroneController.allPhotosByLoggedInUser);
+    app.get('/api/myPhotos', DroneController.allPhotosByLoggedInUser);
 
-    app.post('/api/newPhoto',authenticate, DroneController.createPhoto);
+    app.post('/api/newPhoto', DroneController.createPhoto);
 
     app.get('/api/onePhoto/:id', DroneController.findonePhoto);
 
